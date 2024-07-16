89dba83e596233ce6a43a09a335b1797d5e850bd
arista <atd-help@arista.com>
2024-07-16 21:10:19 +0000

    Fixed stuff

--- Setup_CVaaS/gen.py
+++ Setup_CVaaS/gen.py
@@ -3,19 +3,19 @@ import os
 files = os.listdir('configlets')
 
 
-# for file in files:
-#     name, ext = file.split('.')
-#     print(f"  {name}: \"\u007b\u007b lookup('file', '../configlets/Edge10-BASE.cfg') \u007d\u007d\"")
-
-
-
 for file in files:
     name, ext = file.split('.')
-    print(f"  - fqdn: {name}")
-    print(f"    parentContainerName: Tenant")
-    print(f"    configlets:")
-    print(f"      - 'ATD-INFRA'")
-    print(f"      - '{name}'")
+    print(f"  {name}: \"\u007b\u007b lookup('file', '../configlets/{name}-BASE.cfg') \u007d\u007d\"")
+
+
+
+# for file in files:
+#     name, ext = file.split('.')
+#     print(f"  - fqdn: {name}")
+#     print(f"    parentContainerName: Tenant")
+#     print(f"    configlets:")
+#     print(f"      - 'ATD-INFRA'")
+#     print(f"      - '{name}'")
 
 
 #   - fqdn: Edge10-BASE
